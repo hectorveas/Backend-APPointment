fcbbed694cd55ddeeb84d3f021ab57462191cbdf
hectorveas <hveas10@gmail.com>
2020-12-06 20:17:08 -0300

    se actualizaron todos los componentes

--- src/app/components/contactoPersonal/contactoPersonal.schema.ts
+++ src/app/components/contactoPersonal/contactoPersonal.schema.ts
@@ -1,29 +1,16 @@
 import { Schema, model, Document } from "mongoose";
+import  {Contacto} from "../../models/contactoPersonal.model";
 
-interface contactoPersonalDoc extends Document {
-  _id: string;
-  nombrePaciente: string;
-  rut: string;
-  email: string;
-  contrasena: string;
-  telefono: string;
-  confirmacion: boolean;
-  fechaSolicitud: Date;
-}
-
-const schema: Schema<contactoPersonalDoc> = new Schema({
-  nombre: { type: String, required: true },
-  apellido: { type: String, required: true },
+const definition: Partial<Record<keyof Contacto, any>> = {
+  nombrePaciente: { type: String, required: true },
   rut: { type: String, required: true },
-  mail: { type: String, required: true },
+  email: { type: String, required: true },
   contrasena: { type: String, required: false },
   telefono: { type: String, required: false },
   confirmacion: { type: String, required: false },
   fechaSolicitud: { type: Date },
-});
+};
 
-export default model<contactoPersonalDoc>(
-  "ContactoPersonal",
-  schema,
-  "contactoPersonal"
-);
+const schema: Schema<Contacto> = new Schema(definition);
+
+export default model<Contacto & Document>("Contacto", schema, "contacto");
