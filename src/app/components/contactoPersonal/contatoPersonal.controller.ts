92967ffe218234227ebaaf151e7e7d242e5fb8a6
Benjamin Contreras <benjamin.contreras.g@mail.pucv.cl>
2020-12-10 18:56:02 -0300

    ...

--- src/app/components/contactoPersonal/contatoPersonal.controller.ts
+++ src/app/components/contactoPersonal/contatoPersonal.controller.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ContactoPersonal } from "../../models/contactoPersonal.model";
 import contactoPersonalRepository from "./contactoPersonal.repository";
 
@@ -44,50 +43,3 @@ export default {
   buscarContacto,
   modificarContacto,
 };
-=======
-import { ContactoPersonal } from "../../models/contactoPersonal.model";
-import contactoPersonalRepository from "./contactoPersonal.repository";
-
-function crearContacto(contacto: ContactoPersonal) {
-  return contactoPersonalRepository.crearContacto(contacto);
-}
-
-function eliminarContacto(idContacto: string) {
-  return contactoPersonalRepository.eliminarContacto(idContacto);
-}
-
-function mostrarContactos() {
-  return contactoPersonalRepository.mostrarContactos();
-}
-
-function existeContacto(rutContacto: string) {
-  if (contactoPersonalRepository.existeContacto(rutContacto) == null) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function buscarContacto(idContacto: string) {
-  return contactoPersonalRepository.buscarContacto(idContacto);
-}
-
-async function modificarContacto(
-  idContacto: string,
-  contactoPersonal: Partial<ContactoPersonal>
-): Promise<Partial<ContactoPersonal | null>> {
-  return contactoPersonalRepository.modificarContacto(
-    idContacto,
-    contactoPersonal
-  );
-}
-
-export default {
-  crearContacto,
-  eliminarContacto,
-  mostrarContactos,
-  existeContacto,
-  buscarContacto,
-  modificarContacto,
-};
->>>>>>> 00545de4a6cc308bba955c8ed7ad1860e270b268
